3659f0b1a04ad1acda2c2ba92af6db8ade64b8d4
Vasil Joskov <v.joskov@xs-software.com>
2015-12-03 14:40:28 +0200

    Initial

--- lib/db/sync.rb
+++ lib/db/sync.rb
@@ -3,5 +3,10 @@ require 'db/sync/version'
 module Db
   module Sync
     # Your code goes here...
+    class Railtie < Rails::Railtie
+      rake_tasks do
+        load File.expand_path('../../tasks/db-sync.rake', __FILE__)
+      end
+    end
   end
 end
