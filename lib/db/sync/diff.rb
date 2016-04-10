3c12ffbd28a386377f202553050b4bc8c6e6d3bd
Vasil Joskov <v.joskov@xs-software.com>
2016-04-11 01:04:21 +0300

    Iteration Based

--- lib/db/sync/diff.rb
+++ lib/db/sync/diff.rb
@@ -1,53 +1,77 @@
 class Db::Sync::Diff
   attr_accessor :original, :replace_with, :pkey
+  attr_reader :inserts, :deletes, :updates
 
   def initialize(original, replace_with, pkey)
-    self.original = original
-    self.replace_with = replace_with
+    self.original = original.collect
+    self.replace_with = replace_with.collect
     self.pkey = pkey
+    check_items
   end
 
-  def inserts
-    results = []
-    # TODO: change to iteration based
-    replace_with.each do |item|
-      found = search(original, item)
-      results << item.dup if found.blank?
-    end
-    results
+  def next_original_item
+    original.next
+  rescue StopIteration
+    nil
   end
 
-  def deletes
-    results = []
-    # TODO: change to iteration based
-    original.each do |item|
-      found = search(replace_with, item)
-      results << item.slice(*pkey) if found.blank?
-    end
-    results
+  def next_replace_item
+    replace_with.next
+  rescue StopIteration
+    nil
   end
 
-  def updates
-    results = []
-    # TODO: change to iteration based
-    original.each do |item|
-      found = search(replace_with, item)
-      next if found.blank?
-      next if found == item
-      changes = {}
-      found.each do |key, value|
-        next if value == item[key]
-        changes[key] = value
+  def check_items
+    @inserts = []
+    @deletes = []
+    @updates = []
+    original.rewind
+    replace_with.rewind
+
+    original_item = next_original_item
+    replace_item = next_replace_item
+
+    loop do
+      cmp = compare(original_item, replace_item)
+      if cmp == 0
+        check_for_update(original_item, replace_item)
+        original_item = next_original_item
+        replace_item = next_replace_item
+      elsif cmp > 0
+        @inserts << replace_item
+        replace_item = next_replace_item
+      else
+        @deletes << original_item.slice(*pkey)
+        original_item = next_original_item
       end
-      results << { key: item.slice(*pkey), changes: changes }
+      break if original_item.nil? && replace_item.nil?
     end
-    results
+    nil
   end
 
-  def search(stack, search_for)
-    stack.each do |item|
-      return item if item.slice(*pkey) == search_for.slice(*pkey)
+  def check_for_update(original_item, replace_item)
+    changes = {}
+    replace_item.each do |key, value|
+      next if value == original_item[key]
+      changes[key] = value
     end
-    false
+    return if changes.blank?
+    @updates << { key: original_item.slice(*pkey), changes: changes }
+  end
+
+  def compare(item1, item2)
+    return -1 if item2.nil?
+    return 1 if item1.nil?
+    self.class.compare(item1.slice(*pkey), item2.slice(*pkey))
+  end
+
+  def self.compare(item1, item2)
+    result = item1.values <=> item2.values
+    compare_count
+    result
+  end
+
+  # TODO: remove, checks for number of comparisons
+  def self.compare_count
   end
 end
--- spec/lib/db/sync/diff_spec.rb
+++ spec/lib/db/sync/diff_spec.rb
@@ -6,21 +6,37 @@ describe Db::Sync::Diff do
       { 'id' => 1, 'title' => 't1', 'body' => 'b1' },
       { 'id' => 2, 'title' => 't2', 'body' => 'b2' },
       { 'id' => 3, 'title' => 't3', 'body' => 'b3' },
+      { 'id' => 5, 'title' => 't5', 'body' => 'b5' },
+      { 'id' => 7, 'title' => 't7', 'body' => 'b7' },
+      { 'id' => 8, 'title' => 't8', 'body' => 'b8' },
+      { 'id' => 9, 'title' => 't9', 'body' => 'b9' }
     ]
   end
   let(:replace_with) do
     [
       { 'id' => 1, 'title' => 't1', 'body' => 'b1' },
       { 'id' => 2, 'title' => 't3', 'body' => 'b2' },
-      { 'id' => 4, 'title' => 't3', 'body' => 'b3' },
+      { 'id' => 5, 'title' => 't5', 'body' => 'b5' },
+      { 'id' => 6, 'title' => 't6', 'body' => 'b6' },
+      { 'id' => 7, 'title' => 't7', 'body' => 'b7' },
+      { 'id' => 8, 'title' => 't8', 'body' => 'b8' },
+      { 'id' => 9, 'title' => 't9', 'body' => 'b9' }
     ]
   end
   let(:pkey) { ['id'] }
   let(:diff) { Db::Sync::Diff.new(original, replace_with, pkey) }
 
+  let(:edge_original) { [{ 'id' => 1, 'data' => 1 }] }
+  let(:edge_replace) { [{ 'id' => 2, 'data' => 1 }] }
+  let(:edge_diff) { Db::Sync::Diff.new(edge_original, edge_replace, pkey) }
+
   describe 'inserts' do
     it 'are correct' do
-      expect(diff.inserts).to eq([{ 'id' => 4, 'title' => 't3', 'body' => 'b3' }])
+      expect(diff.inserts).to eq([{ 'id' => 6, 'title' => 't6', 'body' => 'b6' }])
+    end
+
+    it 'works with edge cases' do
+      expect(edge_diff.inserts).to eq([{ 'id' => 2, 'data' => 1 }])
     end
   end
 
@@ -28,11 +44,24 @@ describe Db::Sync::Diff do
     it 'are correct' do
       expect(diff.deletes).to eq([{ 'id' => 3 }])
     end
+
+    it 'works with edge cases' do
+      expect(edge_diff.deletes).to eq([{ 'id' => 1 }])
+    end
   end
 
   describe 'updates' do
     it 'are correct' do
       expect(diff.updates).to eq([{ key: { 'id' => 2 }, changes: { 'title' => 't3'} }])
+    end
+  end
+
+  describe 'diff' do
+    it 'has reduced iterations' do
+      expect(Db::Sync::Diff).to receive(:compare_count).exactly(8).times
+      diff.updates
+      diff.inserts
+      diff.deletes
     end
   end
 end
